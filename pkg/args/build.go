6610a2b1352491ce76f3cb317a7e95bd0ab1bfdc
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-05-20 10:35:06 -0500

    WIP

---
+++ pkg/args/build.go
@@ -0,0 +1,72 @@
+package args
+
+import (
+	"context"
+
+	cmdv1alpha2 "github.com/unstoppablemango/go-protocmd/gen/dev/unmango/cmd/v1alpha2"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+func FromJson(ctx context.Context, req *cmdv1alpha2.FromJsonRequest) (*cmdv1alpha2.FromJsonResponse, error) {
+	panic("unimplemented")
+}
+
+func FromProto(ctx context.Context, req *cmdv1alpha2.FromProtoRequest) (*cmdv1alpha2.FromProtoResponse, error) {
+	panic("unimplemented")
+}
+
+func FromYaml(ctx context.Context, req *cmdv1alpha2.FromYamlRequest) (*cmdv1alpha2.FromYamlResponse, error) {
+	panic("unimplemented")
+}
+
+func protoField(b Builder, fd protoreflect.FieldDescriptor, v protoreflect.Value) {
+	name := fd.TextName()
+
+	switch {
+	case fd.IsList():
+		listArg(b, name, v.List())
+	case fd.IsMap():
+		mapArg(b, name, v.Map())
+	default:
+		switch fd.Kind() {
+		case protoreflect.BoolKind:
+			Opt(b, name, v.IsValid, v.Bool)
+		case protoreflect.StringKind:
+			Arg(b, name, v.IsValid, v.String)
+		case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind,
+			protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+			Arg(b, name, v.IsValid, v.Int)
+		case protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
+			protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+			Arg(b, name, v.IsValid, v.Uint)
+		case protoreflect.FloatKind, protoreflect.DoubleKind:
+			Arg(b, name, v.IsValid, v.Float)
+		case protoreflect.EnumKind:
+			Arg(b, name, v.IsValid, v.Enum)
+		case protoreflect.BytesKind:
+			Arg(b, name, v.IsValid, v.Bytes)
+		case protoreflect.MessageKind, protoreflect.GroupKind:
+			msgArg(b, v.Message())
+		}
+	}
+}
+
+func listArg(b Builder, name string, l protoreflect.List) {
+	for i := range l.Len() {
+		Append(b, name, l.Get(i))
+	}
+}
+
+func mapArg(b Builder, name string, m protoreflect.Map) {
+	for k, v := range m.Range {
+		Append(b, name, k, v)
+	}
+}
+
+func msgArg(b Builder, msg protoreflect.Message) {
+	for fd, v := range msg.Range {
+		// How does protobuf handle recursion?
+		// Do we need to a base case?
+		protoField(b, fd, v)
+	}
+}
