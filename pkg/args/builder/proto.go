81586c4c9c9b6584a857d3131ba31fefb182e1b1
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-05-20 15:49:43 -0500

    WIP

--- pkg/args/builder/proto.go
+++ pkg/args/builder/proto.go
@@ -7,45 +7,52 @@ func ProtoField(fd protoreflect.FieldDescriptor, v protoreflect.Value) (args []s
 
 	switch {
 	case fd.IsList():
-		return ProtoList(name, v.List())
+		return ProtoList(fd, v.List())
 	case fd.IsMap():
 		return ProtoMap(name, v.Map())
 	}
-
-	switch fd.Kind() {
-	case protoreflect.BoolKind:
-		return Opt(name, v.IsValid, v.Bool)
-	case protoreflect.StringKind:
-		return Arg(name, v.IsValid, v.String)
-	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind,
-		protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
-		return Arg(name, v.IsValid, v.Int)
-	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
-		protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
-		return Arg(name, v.IsValid, v.Uint)
-	case protoreflect.FloatKind, protoreflect.DoubleKind:
-		return Arg(name, v.IsValid, v.Float)
-	case protoreflect.EnumKind:
-		return Arg(name, v.IsValid, v.Enum)
-	case protoreflect.BytesKind:
-		return Arg(name, v.IsValid, v.Bytes)
-	case protoreflect.MessageKind, protoreflect.GroupKind:
-		return ProtoMessage(v.Message())
-	}
-
-	panic("unsupported: " + fd.FullName())
+	return ProtoValue(fd, v)
 }
 
-func ProtoList(name string, l protoreflect.List) (args []string) {
-	for i := range l.Len() {
-		args = append(args, name, l.Get(i))
+func ProtoValue(fd protoreflect.FieldDescriptor, value protoreflect.Value) []string {
+	switch kind := fd.Kind(); kind {
+	case protoreflect.BoolKind:
+		return Opt(name(fd), value.IsValid, value.Bool)
+	case protoreflect.StringKind:
+		return Arg(name(fd), value.IsValid, value.String)
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind,
+		protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return Arg(name(fd), value.IsValid, value.Int)
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
+		protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return Arg(name(fd), value.IsValid, value.Uint)
+	case protoreflect.FloatKind, protoreflect.DoubleKind:
+		return Arg(name(fd), value.IsValid, value.Float)
+	case protoreflect.EnumKind:
+		return Arg(name(fd), value.IsValid, value.Enum)
+	case protoreflect.BytesKind:
+		return Arg(name(fd), value.IsValid, value.Bytes)
+	case protoreflect.MessageKind, protoreflect.GroupKind:
+		return ProtoMessage(value.Message())
+	default:
+		panic("unsupported kind: " + kind.String())
+	}
+}
+
+func ProtoList(fd protoreflect.FieldDescriptor, list protoreflect.List) (args []string) {
+	for i := range list.Len() {
+		v := ProtoValue(fd, list.Get(i))
+		args = append(args, v...)
 	}
 	return
 }
 
-func ProtoMap(name string, m protoreflect.Map) (args []string) {
-	for k, v := range m.Range {
-		args = append(args, k, v)
+func ProtoMap(fd protoreflect.FieldDescriptor, m protoreflect.Map) (args []string) {
+	kt := fd.MapKey().Kind()
+	vt := fd.MapValue().Kind()
+
+	for key, value := range m.Range {
+		args = append(args, key, value)
 	}
 	return
 }
@@ -55,4 +62,8 @@ func ProtoMessage(msg protoreflect.Message) (args []string) {
 		args = append(args, ProtoField(fd, v)...)
 	}
 	return
+}
+
+func name(fd protoreflect.FieldDescriptor) string {
+	return fd.TextName()
 }
