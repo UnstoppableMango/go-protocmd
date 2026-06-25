5dff54d41dd48aa03f3bcafc0a825570ccc2d1f1
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-24 22:33:14 -0500

    WIP

--- pkg/cli/ast.go
+++
@@ -1,3 +0,0 @@
-package cli
-
-type Node any
--- pkg/cli/parse.go
+++ pkg/cli/parse.go
@@ -1,24 +1,7 @@
 package cli
 
-import (
-	"fmt"
+import cliv1alpha1 "github.com/unstoppablemango/go-protocmd/gen/unmango/cli/v1alpha1"
 
-	cliv1alpha1 "github.com/unstoppablemango/go-protocmd/gen/unmango/cli/v1alpha1"
-	"github.com/unstoppablemango/go-protocmd/pkg/cli/proto"
-)
-
-func Parse(req *cliv1alpha1.ParseRequest) (*cliv1alpha1.ParseResponse, error) {
-	var err error
-	res := &cliv1alpha1.ParseResponse_builder{}
-	switch in := req.WhichInput(); in {
-	case cliv1alpha1.ParseRequest_Proto_case:
-		res.Utility, err = proto.Parse(req.GetProto())
-	default:
-		return nil, fmt.Errorf("unsupported input: %s", in)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-	return res.Build(), nil
+func Parse(cmdline string) (*cliv1alpha1.CommandLine, error) {
+	return nil, nil
 }
